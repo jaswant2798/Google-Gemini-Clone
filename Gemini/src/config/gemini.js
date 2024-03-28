41bbba1a68f28a79994dc29d8e223c7aec710d3a
jaswant2798 <147206104+jaswant2798@users.noreply.github.com>
2024-03-28 13:18:44 -0400

    Update gemini.js

--- Gemini/src/config/gemini.js
+++ Gemini/src/config/gemini.js
@@ -11,7 +11,7 @@ import {
   } from "@google/generative-ai";
   
   const MODEL_NAME = "gemini-1.0-pro";
-  const API_KEY = "AIzaSyBBh0HqKDbKrfWTizPAZXAMxyCIzDS81CI";
+  const API_KEY = "user your own keys";
   
   async function runChat(promt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
