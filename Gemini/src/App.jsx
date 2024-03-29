58ddf03f0c0fde5e76dbbb22cd83c9d3e85212d3
jaswant2798 <jaswantsingh2798@gmail.com>
2024-03-28 23:24:09 -0400

    add files

---
+++ Gemini/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./component/sidebar/sidebar";
+import Main from "./component/main/Main";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
